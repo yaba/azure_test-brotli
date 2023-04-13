14f6e9dd709260058d5e73c642b65ab86517af94
yaba <polido@gmail.com>
2023-04-13 12:00:32 +0100

    test

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from flask_compress import Compress
+
+app = Flask(__name__)
+# Set the alogirthm to apply gzip - https://github.com/colour-science/flask-compress#options
+app.config['COMPRESS_ALGORITHM'] = 'gzip'
+Compress(app)
+
+
+@app.route("/")
+def index():
+    msg = 'gzip-stack-examples-python-gzip'
+    return jsonify({ 'message': msg * 30 })

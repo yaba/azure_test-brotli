ad5998f8c02d2ff2eb52903eef73e27c1dab8746
yaba <polido@gmail.com>
2023-04-13 12:08:04 +0100

    enable brotli

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_compress import Compress
 
 app = Flask(__name__)
 # Set the alogirthm to apply gzip - https://github.com/colour-science/flask-compress#options
-app.config['COMPRESS_ALGORITHM'] = 'gzip'
+app.config['COMPRESS_ALGORITHM'] = 'br'
 Compress(app)
 
 
